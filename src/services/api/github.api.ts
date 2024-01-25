e36d1a864906dc1b384fe9755ef096e4d6054912
Pedro Lima <pedro.oficial1969@gmail.com>
2024-01-25 13:54:20 -0300

    formatação.

--- src/services/api/github.api.ts
+++ src/services/api/github.api.ts
@@ -2,11 +2,7 @@ import { IGithubUser } from "@/interfaces/IGithubUser";
 
 export const getUserGithub = async (): Promise<IGithubUser> => {
   const response = await fetch("https://api.github.com/users/pedropdin");
-
   const data = await response.json();
-  
   const { avatar_url, html_url } = data;
-
-
   return { avatar_url, html_url }
 }
