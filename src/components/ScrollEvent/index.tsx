a8bd6283642a09345bdc5661f2727960ed24290f
Pedro Lima <pedro.oficial1969@gmail.com>
2024-01-25 13:53:31 -0300

    alteração de acesso ao caminho.

---
+++ src/components/ScrollEvent/index.tsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { ReactNode, useEffect } from "react";
+import { useMainContext } from "@/hooks/useMainContext";
+
+interface Props {
+  children: ReactNode
+}
+
+export const ScrollEvent = ({ children }: Props) => {
+  const { setIsOpenNavBar } = useMainContext();
+
+  useEffect(() => {
+    document.addEventListener('scroll', () => setIsOpenNavBar(false))
+  }, []);
+
+  return (
+    <>
+      {children}
+    </>
+  )
+}
