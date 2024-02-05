09896fa265630f99b7cecc138cf65fe574397133
Pedro Lima <pedro.oficial1969@gmail.com>
2024-02-05 16:57:12 -0300

    alteração minimalista

--- src/components/MapUser/Map.tsx
+++ src/components/MapUser/Map.tsx
@@ -32,6 +32,7 @@ export default function Map({position, street}: Props) {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      
       <Marker
         icon={
           new L.Icon({
