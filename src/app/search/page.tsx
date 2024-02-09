ee45c84f85c268749415edd380c3a625ec9e9d87
Pedro Lima <pedro.oficial1969@gmail.com>
2024-02-09 17:53:49 -0300

    :sparkles: Page Seach Partial
    Add pagina de busca parcialmente ainda em desenvolvimento a integracao
     do controle de paginacao.

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,21 +1,40 @@
 import { getSearchUser } from "@/services/api/randomUser.api";
-import { ProfilesUsers } from "@/components/ProfilesUsersPagination";
+import { ProfilesUsers } from "@/components/ProfilesUsers";
+import nationalitiesList from "@/utils/nationalities";
 
 export default async function Search({
   searchParams
-}: { searchParams: { nat: string, gender: string, results: string } }) {
-  const { data, page, seed } = await getSearchUser(+searchParams.results, searchParams.nat, searchParams.gender);
-  // console.log(data)
+}: {
+    searchParams: {
+      nat: string, gender: string, results: string
+    }
+}) {
+  const { data, page, seed } = await getSearchUser(searchParams.results, searchParams.nat, searchParams.gender);
+  const genderUser: string = searchParams.gender === "female" ? "Feminino" : "Masculino";
+  const nationalityUser: string = nationalitiesList
+    .find((natUser) => natUser.abbreviated === searchParams.nat)?.name as string;
 
   return (
-    <main className="mt-28">
-      <div>
+    <main className="mt-24 pb-0 flex flex-col items-center justify-center">
+      <div className="ml-0 w-[70%] mb-8">
+        <p className="font-normal text-[13px]">
+          <span className="font-bold pr-1 text-[#83ba43]">Resultado da pequisa:</span>
+          {`Gênero ${genderUser} - Nacionalidade ${nationalityUser} - Quantidade de usuários ${searchParams.results}.`}
+        </p>
+      </div>
+
+      <hr />
+
+      <div className="p-8 bg-white shadow-sm border-solid border-[1px] border-[rgba(0,0,0,.25)] rounded-sm">
         <ProfilesUsers
           data={data}
           page={page as number}
           limit={+searchParams.results}
           seed={seed as string}
           valueColumns={4}
+          isSearch={true}
+          nat={searchParams.nat}
+          gender={searchParams.gender}
         />
       </div>
     </main>
