217e1f23b2bf78237fd44ad6b7bb9918214c030c
Pedro Lima <pedro.oficial1969@gmail.com>
2024-02-11 18:16:28 -0300

    :iphone: Page User tablet

--- src/components/InfoUser/index.tsx
+++ src/components/InfoUser/index.tsx
@@ -24,16 +24,16 @@ export default function InfoUser({ picture, name, gender, age, phone, cell, emai
           alt={`${name} profile`}
           width={190}
           height={190}
-          className="rounded-full p-1 border-solid border-2 border-[rgba(0,0,0,.25)] z-10 bg-white max-micro-screen:w-[120px] max-micro-screen:h-[120px] micro-screen:max-mobile:w-[130px] micro-screen:max-mobile:h-[130px]"
+          className="rounded-full p-1 border-solid border-2 border-[rgba(0,0,0,.25)] z-10 bg-white mobile:max-tablet:w-[140px] mobile:max-tablet:h-[140px]max-micro-screen:w-[120px] max-micro-screen:h-[120px] micro-screen:max-mobile:w-[130px] micro-screen:max-mobile:h-[130px]"
         />
 
-        <p className="flex flex-col text-center font-bold text-xl max-micro-screen:text-[18px]">
+        <p className="flex flex-col text-center font-bold text-xl mobile:max-tablet:text-[18px] mobile:max-tablet:pt-2 max-micro-screen:text-[18px]">
           <span className="text-[#999]">Olá, meu nome é</span>
           {name}
         </p>
       </div>
 
-      <div className="border-separate border-2 border-[rgba(0,0,0,.25)] h-[200px] rounded-full dark:border-white max-micro-screen:hidden micro-screen:max-mobile:hidden"></div>
+      <div className="border-separate border-2 mobile:max-tablet:border-1 border-[rgba(0,0,0,.25)] h-[200px] rounded-full dark:border-white max-micro-screen:hidden micro-screen:max-mobile:hidden"></div>
 
       <div className="grid gap-3 max-micro-screen:w-[90%] micro-screen:max-mobile:w-[90%] max-micro-screen:text-[14px]">
         <div className="grid gap-1">
