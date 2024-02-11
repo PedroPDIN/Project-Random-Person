232e7d5ae81740e0700a4101da08bddb617dc7d1
Pedro Lima <pedro.oficial1969@gmail.com>
2024-02-11 14:03:13 -0300

    :sparkles: Add DarkMode
    adiciona metodo de darkmode.

---
+++ src/components/DarkModeBtn/index.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState, useEffect } from "react";
+import { MdDarkMode } from "react-icons/md";
+import { HiSun } from "react-icons/hi";
+import { useMainContext } from "@/hooks/useMainContext";
+
+export default function DarkModeBtn() {
+  const [theme, setTheme] = useState<string | null>(localStorage.getItem('theme') !== "light" ? "dark" : "light");
+  const { setThemeGlobal } = useMainContext();
+
+  useEffect(() => {
+      const root = document.documentElement.classList;
+  
+      root.remove(theme === "light" ? "dark" : "light");
+      root.add(theme as string);
+      localStorage.setItem("theme", theme as string);
+      setThemeGlobal(theme as string)
+  }, [theme])
+  
+  return (
+    <div className="curso-pointer text-white">
+      {theme === "light" ? (
+        <button onClick={() => setTheme("dark")}>
+          <HiSun />
+        </button>
+      ) : (
+          <button onClick={() => setTheme("light")}>
+            <MdDarkMode />
+          </button>
+      )}
+    </div>
+  )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,34 +3,41 @@
 import { MdOutlineMenu } from "react-icons/md";
 import { FaArrowLeft } from "react-icons/fa";
 import { useMainContext } from "@/hooks/useMainContext";
-
+import DarkModeBtn from "../DarkModeBtn";
 export default function Header() {
   const { isOpenNavBar, toggleIsOpen, title } = useMainContext();
 
   return (
     <header className="bg-[#2c2e31] text-white relative h-20 z-0">
-      <div className="flex">
-        {isOpenNavBar ? (
-          <button
-            className="z-10 h-full absolute flex items-center left-14"
-            onClick={toggleIsOpen}
-          >
-            <FaArrowLeft className="text-4xl hover:bg-[#484b50] rounded-md p-1 transition" />
-          </button>
-        ) : (
-          <button
-            className="z-10 h-full absolute flex items-center left-14"
-            onClick={toggleIsOpen}
-          >
+      <div className="flex justify-between pt-8 px-20">
+        <div>
+          {isOpenNavBar ? (
+            <button
+              className="z-10 h-full flex items-center"
+              onClick={toggleIsOpen}
+            >
+              <FaArrowLeft className="text-4xl hover:bg-[#484b50] rounded-md p-1 transition" />
+            </button>
+          ) : (
+            <button
+              className="z-10 h-full flex items-center"
+              onClick={toggleIsOpen}
+            >
               <MdOutlineMenu className="text-4xl hover:bg-[#484b50] rounded-md p-1 transition" />
-          </button>
-        )}
+            </button>
+          )}
+        </div>
 
-        <div className="w-full h-full flex justify-center absolute z-0">
+        <div className="h-full z-0">
           <h1 className="text-3xl font-bold flex items-center text-center">{ title }</h1>
         </div>
+
+        <div className="text-4xl">
+          <DarkModeBtn />
+        </div>
       </div>
-      <div className="p-16 bg-[#2c2e31] shadow-2xl"></div>
+
+      <div className="w-full py-8 bg-[#2c2e31] shadow-2xl"></div>
     </header>
   )
 }
